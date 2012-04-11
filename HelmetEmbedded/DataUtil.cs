0d93180b48ed3a7a131f883f4e5909fdf7c8cf9d
TELMaH P1 Grupp 6 <m10p5376@student.mah.se>
2012-04-11 14:17:53 +0200

    Added DataUtil class.

---
+++ HelmetEmbedded/DataUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.SPOT;
+
+namespace Helmet
+{
+    class DataUtil
+    {
+        public static byte[] accDataToJson(double x, double y, double z)
+        {
+            // TODO Optimize?
+            string json = "";
+            json += "{\"accX\":" + x.ToString() + ",";
+            json += "\"accY\":" + y.ToString() + ",";
+            json += "\"accZ\":" + z.ToString() + "}";
+            return stringToByteArray(json);
+        }
+
+        public static byte[] stringToByteArray(string input)
+        {
+            byte[] byteData = new byte[input.Length];
+            for (int i = 0; i < input.Length; ++i)
+                byteData[i] = (byte) input[i];
+            return byteData;
+        }
+    }
+}
