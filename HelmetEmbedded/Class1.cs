eb88d1ceba1f2636bc049b0d9117754725ec180c
TELMaH P1 Grupp 6 <m10p5376@student.mah.se>
2012-04-11 11:15:18 +0200

    Created AcidentDetection class.

---
+++ HelmetEmbedded/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.SPOT;
+
+namespace Helmet
+{
+    class AccidentDetection
+    {
+        private double sumThreshold;
+
+        public AccidentDetection(double sumThreshold)
+        {
+            this.sumThreshold = sumThreshold;
+        }
+
+        public bool addData(double x, double y, double z, bool freeFall)
+        {
+            if ((x + y + z) > sumThreshold)
+                return true;
+            return
+                false;
+        }
+
+        
+    }
+}
